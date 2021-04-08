d7318e68850138b6b6cf991cfa8421ef426f868c
ChenTanyi <chentanyi@gmail.com>
2021-04-08 16:11:59 +0800

    feat: add key vault

--- go.mod
+++ go.mod
@@ -3,6 +3,10 @@ module github.com/tanyi-test/azure-webapp-connection-test
 go 1.13
 
 require (
+	github.com/Azure/azure-sdk-for-go v53.0.0+incompatible
+	github.com/Azure/go-autorest/autorest/azure/auth v0.5.7
+	github.com/Azure/go-autorest/autorest/to v0.4.0 // indirect
+	github.com/Azure/go-autorest/autorest/validation v0.3.1 // indirect
 	github.com/denisenkom/go-mssqldb v0.9.0
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-sql-driver/mysql v1.6.0
--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
 	"crypto/tls"
 	"crypto/x509"
 	"database/sql"
+	"fmt"
 
+	"github.com/Azure/azure-sdk-for-go/profiles/latest/keyvault/keyvault"
+	"github.com/Azure/azure-sdk-for-go/services/keyvault/auth"
 	_ "github.com/denisenkom/go-mssqldb"
 	"github.com/gin-gonic/gin"
 	"github.com/go-sql-driver/mysql"
@@ -19,6 +23,39 @@ func post(c *gin.Context) {
 	tp := c.PostForm("type")
 	conn := c.PostForm("connection")
 
+	if tp == "keyvault" {
+		keyvaultConnect(conn, c)
+	} else {
+		dbConnect(tp, conn, c)
+	}
+
+}
+
+func keyvaultConnect(name string, c *gin.Context) {
+	authorizer, err := auth.NewAuthorizerFromEnvironment()
+	if err != nil {
+		c.String(500, "Auth Error: %s", err.Error())
+		return
+	}
+
+	client := keyvault.New()
+	client.Authorizer = authorizer
+
+	secrets, err := client.GetSecrets(context.Background(), fmt.Sprintf("https://%s.vault.azure.net", name), nil)
+	if err != nil {
+		c.String(500, "List Secrets Error: %s", err.Error())
+		return
+	}
+
+	for _, secret := range secrets.Values() {
+		c.String(200, "First Secret ID: %s", *secret.ID)
+		return
+	}
+
+	c.String(202, "Not Secret Found")
+}
+
+func dbConnect(tp, conn string, c *gin.Context) {
 	db, err := sql.Open(tp, conn)
 	if err != nil {
 		c.String(500, "Connect Error: %s", err.Error())
